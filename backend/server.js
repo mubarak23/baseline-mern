f993c2f0d3cd9bb9106b4f3bcc47a94474ac4e6d
mubarak23 <mubarakaminu340@gmail.com>
2023-05-18 07:08:14 +0100

    base line project setup for mern stack app

---
+++ backend/server.js
@@ -0,0 +1,32 @@
+import cookieParser from 'cookie-parser';
+import dotenv from 'dotenv';
+import express from 'express';
+import connectDB from './config/db.js';
+import { errorHandler, notFound } from './middleware/errorMiddleaware.js';
+import userRoutes from './routes/userRoutes.js';
+dotenv.config();
+
+
+const port = process.env.PORT || 5000
+
+connectDB()
+
+const app = express()
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cookieParser());
+
+
+app.use('/api/users', userRoutes);
+
+app.get('/', (req, res) => {
+    res.send('API is running....');
+  });
+  
+
+app.use(notFound);
+app.use(errorHandler);  
+
+app.listen(port, () => console.log(`Server Running Port ${port}`))  
