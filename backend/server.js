565f6dd3c7511e503862a5c37a54e6ac6445b60f
mubarak23 <mubarakaminu340@gmail.com>
2023-05-24 16:18:11 +0100

    build app

--- backend/server.js
+++ backend/server.js
@@ -21,9 +21,18 @@ app.use(cookieParser());
 
 app.use('/api/users', userRoutes);
 
-app.get('/', (req, res) => {
+if (process.env.NODE_ENV === 'production') {
+  const __dirname = path.resolve();
+  app.use(express.static(path.join(__dirname, '/frontend/dist')));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'))
+  );
+} else {
+  app.get('/', (req, res) => {
     res.send('API is running....');
   });
+}
   
 
 app.use(notFound);
